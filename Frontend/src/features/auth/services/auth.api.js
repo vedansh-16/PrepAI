10f61cd1307d391da304f07f7aec0256800f4a51
Vedansh <vedanshjakhmola@gmail.com>
2026-08-24 00:02:16 +0530

    Configure production API and CORS

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://prepai-backend-ui8o.onrender.com",
     withCredentials: true
 })
 
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://prepai-backend-ui8o.onrender.com",
     withCredentials: true,
 })
 
